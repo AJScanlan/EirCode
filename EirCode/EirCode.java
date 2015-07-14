ab372f3bb7b58cad9975f463c904c83b47906515
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-14 10:05:00 +0100

    Initial commit

---
+++ EirCode/EirCode.java
@@ -0,0 +1,42 @@
+package com.codechallenge.eircode;
+
+import java.util.Scanner;
+
+public class EirCode {
+
+	public static void main(String[] args) {
+		String[] eirArray = {"D02 Y006", "D04 C932", "D15 XR2R", "D03 RR27", "D24 H510"};
+		String[] addressArray = {"5 Merrion Square North, Dublin 2", "10 Burlington Road, Dublin 4", "Dunsink Observatory, Dunsink Lane, Dublin 15", "26 KINCORA ROAD, Clontarf, Dublin 3", "Partas, 4A BROOKFIELD ENTERPRISE CENTRE, Dublin 24"};
+	
+		
+		
+	
+		boolean quit = true;
+		boolean found = false;
+		
+		do{
+			String input = "";
+			System.out.println("Please input EirCode");
+						
+			Scanner scan = new Scanner(System.in);
+		
+			input = scan.nextLine();
+
+			for(int i = 0; i < eirArray.length; ++i){
+				if(input.equalsIgnoreCase(eirArray[i])){
+					System.out.println("The address you're looking for is: " + addressArray[i]);
+					found = true;
+				}
+			}
+			
+			if(!found) System.out.println("Address not found");
+			
+			System.out.println("Would you like to continue? Y / N");
+			String choice = scan.next();
+			if(choice.equalsIgnoreCase("N")) quit = false;
+						
+		} while (quit);
+			
+	}
+
+}
